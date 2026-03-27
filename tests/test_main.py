740f5c1092fbcaa005ec7167cc0336e3dcd23cf0
righibe <bernardomicolrighi@outlook.com>
2026-03-27 15:21:55 -0300

    coloquei o teste de subtração

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,11 @@
 from src.main import add
+from src.main import subtract
 
 def test_add_function():
     assert add(1,2) == 3
     assert add(0,0) == 0
     assert add(5,5) == 10
+def test_subtract_function():
+    assert subtract(1,2) == -1
+    assert subtract(5,1) == 4
+    assert subtract(7,2) == 5
